306298483897f5cd43719bee1c5539ba0bce03f0
Bhawani Singh <bs190520@gmail.com>
2025-12-02 22:51:05 +0530

    feat: improve styling and structure of ProjectCard component

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -24,41 +24,54 @@ export const ProjectCard = ({
   githubLink,
 }: ProjectCardProps) => {
   return (
-    <Card className='flex flex-row justify-between md:items-center gap-5 p-4 max-md:flex-col max-md:gap-2 w-full border dark:border-neutral-800 dark:hover:border-neutral-700 transition-colors duration-200'>
+    <Card className="flex w-full flex-row justify-between gap-5 border p-4 transition-colors duration-200 max-md:flex-col max-md:gap-2 md:items-center dark:border-neutral-800 dark:hover:border-neutral-700">
       {/* Left: Image */}
-      <div className='relative h-fit select-none [&_img]:aspect-1200/630 [&_img]:rounded-xl'>
+      <div className="relative h-fit select-none [&_img]:aspect-1200/630 [&_img]:rounded-xl">
         <Image
           src={image}
           alt={`${title} Image`}
           width={300}
           height={200}
-          className='w-full md:w-[300px] md:h-[190px] object-cover'
+          className="w-full object-cover md:h-[190px] md:w-[300px]"
         />
-        <div className='pointer-events-none absolute inset-0 rounded-xl ring-1 ring-black/10 ring-inset dark:ring-white/10' />
+        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-black/10 ring-inset dark:ring-white/10" />
       </div>
 
       {/* Right: Text content */}
-      <div className='flex-1/2'>
-        <div className='flex items-center justify-between tracking-wider'>
+      <div className="flex-1/2">
+        <div className="flex items-center justify-between tracking-wider">
           {/* Project Title */}
-          <div className='text-xl font-medium tracking-wide'>{title}</div>
+          <div className="text-xl font-medium tracking-wide">{title}</div>
           {/* Links */}
-          <div className='flex gap-1 text-sm'>
-            <a target='_blank' rel='noopener noreferrer' href={liveLink}>
+          <div className="flex gap-1 text-sm">
+            <a
+              target={liveLink === "building" ? "" : "_blank"}
+              rel="noopener noreferrer"
+              href={liveLink === "building" ? "#" : liveLink}
+            >
               <Button
                 variant={"secondary"}
-                className={`rounded ${liveLink === "building" ? "bg-green-400 text-black " : ""} gap-1 h-5 has-[>svg]:px-1 text-xs mr-1 cursor-pointer hover:bg-zinc-800/50`}
+                className={`rounded ${liveLink === "building" ? "bg-green-400 text-black" : ""} mr-1 h-5 cursor-pointer gap-1 text-xs hover:bg-zinc-800/50 has-[>svg]:px-1`}
               >
-                { liveLink === "building" ? <></> : <ExternalLink className='size-3' /> }
+                {liveLink === "building" ? (
+                  <></>
+                ) : (
+                  <ExternalLink className="size-3" />
+                )}
                 {liveLink === "building" ? "Building..." : "Live"}
               </Button>
             </a>
-            <span className='font-bold'>|</span>
-            <a target='_blank' rel='noopener noreferrer' href={githubLink}>
+            <span className="font-bold">|</span>
+            <a
+              target={githubLink === "private" ? "" : "_blank"}
+              rel="noopener noreferrer"
+              href={githubLink === "private" ? "#" : githubLink}
+            >
               <Button
                 variant={"secondary"}
-                className={`rounded gap-1 ${githubLink === "private" ? "text-amber-700" : ""} h-5 has-[>svg]:px-1 text-xs cursor-pointer hover:bg-zinc-800/50'`}              >
-                <FaGithub className='size-3' />
+                className={`gap-1 rounded ${githubLink === "private" ? "text-amber-700" : ""} hover:bg-zinc-800/50' h-5 cursor-pointer text-xs has-[>svg]:px-1`}
+              >
+                <FaGithub className="size-3" />
                 {githubLink === "private" ? "Private" : "Github"}
               </Button>
             </a>
@@ -66,20 +79,20 @@ export const ProjectCard = ({
         </div>
 
         {/* Description */}
-        <p className='my-2 text-sm leading-tight dark:text-muted-foreground'>
+        <p className="dark:text-muted-foreground my-2 text-sm leading-tight">
           {description}
         </p>
 
         {/* Tech Used */}
-        <p className='my-1 text-sm font-medium dark:text-neutral-200'>
+        <p className="my-1 text-sm font-medium dark:text-neutral-200">
           Technologies Used:
         </p>
-        <div className='mt-1.5 flex flex-wrap gap-1'>
+        <div className="mt-1.5 flex flex-wrap gap-1">
           {tech.map((tech) => (
             <Badge
               key={tech}
               variant={"secondary"}
-              className='items-center gap-0.5 rounded px-1 py-0.5 text-xs tracking-normal transition-colors duration-300 hover:bg-secondary/50 dark:text-neutral-200'
+              className="hover:bg-secondary/50 items-center gap-0.5 rounded px-1 py-0.5 text-xs tracking-normal transition-colors duration-300 dark:text-neutral-200"
             >
               {tech}
             </Badge>
