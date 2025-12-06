e74e94516055f07d411c589b03cd95649dfd8477
Bhawani Singh <bs190520@gmail.com>
2025-12-07 00:50:22 +0530

    feat: update sitemap URL in robots.txt for correct path

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "/sitemap.xml",
+    sitemap: "bhawanibytes.dev/sitemap.xml",
   };
 }
