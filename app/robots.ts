bc7ca91207df5ec3b5252df9b2831678be59a25b
Bhawani Singh <bs190520@gmail.com>
2025-12-07 14:21:34 +0530

    fix: update sitemap URL to include protocol in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "bhawanibytes.dev/sitemap.xml",
+    sitemap: "https://bhawanibytes.dev/sitemap.xml",
   };
 }
